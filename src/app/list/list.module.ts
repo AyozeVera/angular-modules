d70ab7b1c60f77f4b4966b8700c4d2be36b05e08
Ayoze Vera <ayoze@2coders.com>
2019-05-25 19:46:11 +0100

    list service only on list module

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from '../list/list.component';
 import { ListItemsComponent } from '../list/list-items/list-items.component';
 import { CurrentItemComponent } from '../list/current-item/current-item.component';
+import { ListService } from '../list/list.service';
 
 @NgModule({
   declarations: [ ListComponent, ListItemsComponent, CurrentItemComponent],
   imports: [ CommonModule ],
   exports: [ ListComponent ],
-
+  providers: [ ListService ],
 })
 export class ListModule { }
--- src/app/list/list.service.ts
+++ src/app/list/list.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Observable, Subject } from 'rxjs'
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ListService {
   currentSubject: Subject<Object> = new Subject<Object>();
   itemsSubject: Subject<any> = new Subject<any>();
